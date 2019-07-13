40d5c5be9623e705fc8c5113bd1f6e5bcfbd0590
adalll <adalll@mail.ru>
2019-07-13 15:05:25 +0700

    Work 1 After Chapter 23

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends Component {
+
+    render() {
+        return (
+            <ColorContext.Consumer>
+                {(colorValue)=>
+                    <button className={`ui button ${colorValue}`}>
+                        <LanguageContext.Consumer>
+                            {(value)=> value === 'english' ? 'Submit' : 'Voorlaggen'}
+                        </LanguageContext.Consumer>
+                    </button>
+                }
+            </ColorContext.Consumer>
+        );
+    }
+}
+
+export default Button;
